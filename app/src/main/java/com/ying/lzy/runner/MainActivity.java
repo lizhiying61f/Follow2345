b3cb5e83f4a26b86c3e01f111da8dd622847b011
lizhiying61f <382091679@qq.com>
2016-08-07 18:56:51 +0800

    nothing

--- app/src/main/java/com/ying/lzy/runner/MainActivity.java
+++ app/src/main/java/com/ying/lzy/runner/MainActivity.java
@@ -2,7 +2,7 @@ package com.ying.lzy.runner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
